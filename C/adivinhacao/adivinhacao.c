a8cc0298efbe47ae73eff0e859f39679f3aaaccb
Marcos Willian <markusuuuu@gmail.com>
2021-09-26 23:46:23 -0300

    retomando...

--- C/adivinhacao/adivinhacao.c
+++ C/adivinhacao/adivinhacao.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
 
-int main() {
+// tudo que começa com # é uma diretiva
+// a diretiva "define" possibilita a criação de constantes
+// #define NUMERO_DE_TENTATIVAS 3
 
+int main() {
   printf("******************************************\n");
   printf("* Bem vindo ao nosso jogo de adivinhação *\n");
   printf("******************************************\n");
 
   int secretNumber = 42;
   int userNumber;
+  int currentTry = 1;
 
-  printf("Qual é o seu chute? ");
-  scanf("%d", &userNumber);
-  printf("seu chute foi %d", userNumber);
+  while (1) {
+    printf("Tentativa %d\n", currentTry);
+    printf("Qual é o seu chute? ");
 
+    scanf("%d", &userNumber);
+    printf("Seu chute foi %d\n", userNumber);
+
+    if (userNumber < 0) {
+      printf("Você não pode chutar números negativos!\n");
+      continue;
+    }
+
+    int acertou = userNumber == secretNumber;
+    int maior = userNumber > secretNumber;
+
+    if (acertou) {
+      printf("Parabéns, você acertou na tentativa de número %d\n", currentTry);
+      printf("Jogue de novo, você é um bom jogador!\n");
+      break;
+    } else if (maior) {
+      printf("Seu chute foi maior que o número secreto\n");
+    } else {
+      printf("Seu chute foi menor que o número secreto\n");
+    }
+
+    currentTry++;
+  }
+
+  printf("Fim de jogo!\n");
 }
