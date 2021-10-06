788a01cffd6e20de92f6a92c9771530e3ef9df7b
Marcos Willian <markusuuuu@gmail.com>
2021-10-06 00:28:38 -0300

    feat: add customer

--- NodeJS/FINAPI/src/index.js
+++ NodeJS/FINAPI/src/index.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+
+  const findCustomerWithSameCPF = customers.findIndex(c => c.cpf === cpf);
+
+  if (findCustomerWithSameCPF !== -1) {
+    return response.status(400).json({ message: 'Customer already exists!' });
+  }
+
+  const newCustomer = { id: uuidv4(), name, cpf, statement: [] };
+  customers.push(newCustomer);
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
