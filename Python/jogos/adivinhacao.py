5d7f5ca8043da0887a6d89770f25526387042830
Marcos Willian <markusuuuu@gmail.com>
2021-12-13 22:16:48 -0300

    [PYTHON 3] - input function always returns a str type

--- Python/jogos/adivinhacao.py
+++ Python/jogos/adivinhacao.py
@@ -1,1 +1,12 @@
-print("helou")
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+secret_number = 42
+
+chute = input("Digite o seu número: ")
+
+if int(chute) == secret_number:
+    print("Parabéns, você acertou!")
+else:
+    print("Você errou!")
