a6a2410af1a151b37900742d91b03cc1a1544c54
Marcos Willian <markusuuuu@gmail.com>
2021-12-14 22:11:53 -0300

    [PYTHON 3] - lopping...

--- Python/jogos/adivinhacao.py
+++ Python/jogos/adivinhacao.py
@@ -4,9 +4,24 @@ print("*********************************")
 
 secret_number = 42
 
-chute = input("Digite o seu número: ")
+tentativas = 3
+tentativa_atual = 1
 
-if int(chute) == secret_number:
-    print("Parabéns, você acertou!")
-else:
-    print("Você errou!")
+while tentativa_atual <= tentativas:
+    print(f"Tentativa {tentativa_atual} de {tentativas}")
+
+    chute = int(input("Digite o seu número: "))
+    maior = chute == secret_number
+    menor = chute < secret_number
+
+    if maior:
+        print("Parabéns, você acertou!")
+        break
+    elif menor:
+        print("Você errou, o número secreto é maior")
+    else:
+        print("Você errou, o número secreto é menor")
+
+    tentativa_atual += 1
+
+print("Fim de jogo")
