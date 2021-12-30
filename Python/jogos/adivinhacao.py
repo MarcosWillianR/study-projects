fc1fb31b18811a8ee97ef729337e03162a93aa15
Marcos Willian <markusuuuu@gmail.com>
2021-12-30 08:42:32 -0300

    feat: change loop from While to For and added new rule for input number

--- Python/jogos/adivinhacao.py
+++ Python/jogos/adivinhacao.py
@@ -3,16 +3,18 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 secret_number = 42
-
 tries = 3
-current_try = 1
 
-while current_try <= tries:
+for current_try in range(1, tries + 1):
     print(f"Tentativa {current_try} de {tries}")
 
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite um número entre 1 e 100: "))
     bigger = chute == secret_number
     less = chute < secret_number
+
+    if chute < 1 or chute > 100:
+        print("Você deve dígitar um número entre 1 e 100")
+        continue
 
     if bigger:
         print("Congratz, you won!")
@@ -21,7 +23,5 @@ while current_try <= tries:
         print("Você errou, o número secreto é maior")
     else:
         print("Você errou, o número secreto é menor")
-
-    current_try += 1
 
 print("Fim de jogo")
