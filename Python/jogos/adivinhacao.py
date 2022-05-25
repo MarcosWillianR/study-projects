c64f874a0296b98a77e98ad62c1d003425e44f64
Marcos Willian <markusuuuu@gmail.com>
2022-05-25 00:25:00 -0300

    study: continue python3 course, giving dificulty levels

--- Python/jogos/adivinhacao.py
+++ Python/jogos/adivinhacao.py
@@ -4,8 +4,31 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-secret_number = round(random.random() * 100)
-tries = 3
+secret_number = random.randrange(1, 101)
+tries = 0
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+level = int(input("Defina o nível: "))
+
+switch = {
+    1: 10,
+    2: 5,
+    3: 3
+}
+
+tries = switch.get(level)
+if tries == None:
+    print("level inválido, escolha 1, 2 ou 3!")
+    exit()
+
+if level == 1:
+    print("Você escolheu o nível Fácil")
+elif level == 2:
+    print("Você escolheu o nível Médio")
+elif level == 3:
+    print("Você escolheu o nível Difícil")
 
 for current_try in range(1, tries + 1):
     print(f"Tentativa {current_try} de {tries}")
