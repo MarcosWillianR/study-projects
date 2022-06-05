cdd45120508f265faf2d9c614b2f2fc6c690b3a5
Marcos Willian <markusuuuu@gmail.com>
2022-06-05 05:54:00 -0300

    feat: improving react skills

---
+++ Javascript/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from './Post';
+
+export function App() {
+  return (
+    <>
+      <Post
+        author="Marcos Willian"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, atque, quod, voluptate facilis voluptates impedit esse dignissimos ea architecto necessitatibus perferendis accusantium eos aliquid? Ipsam molestiae officia exercitationem veniam alias."
+      />
+    </>
+  )
+}
