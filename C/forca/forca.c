f5171d8071a1d934dbf9a4a35e7c884f495d9257
Marcos Willian <markusuuuu@gmail.com>
2022-06-19 22:51:23 -0300

    feat: resuming C learning

---
+++ C/forca/forca.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char palavrasecreta[20];
+  sprintf(palavrasecreta, "MELANCIA");
+  
+  int acertou = 0;
+  int enforcou = 0;
+
+  char chutes[26];
+  int tentativas = 0;
+
+  do {
+    for (int i = 0; strlen(palavrasecreta) > i; i++) {
+      int achou = 0;
+      for (int j = 0; j < tentativas; j++) {
+        if (chutes[j] == palavrasecreta[i]) {
+          achou = 1;
+          break;
+        }
+      }
+      if (achou) {
+        printf("%c ", palavrasecreta[i]);
+      } else {
+        printf("_ ");
+      } 
+    }
+    printf("\n");
+    char chute;
+    printf("Qual letra?");
+    scanf(" %c", &chute); // precisa do espaço para não ler o enter
+    chutes[tentativas] = chute;
+    tentativas++;
+  } while (!acertou && !enforcou);
+}
--- C/forca/main.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  printf("helou %d", 15);
-}
