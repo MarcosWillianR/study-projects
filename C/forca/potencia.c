4a8eda6ff1e6200f2cf13473b5730ad526097cf4
Marcos Willian <markusuuuu@gmail.com>
2022-06-27 00:26:27 -0300

    C study: using pointers on potencia.c

--- C/forca/potencia.c
+++ C/forca/potencia.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-void potencia(int a, int b) {
-  int result = 1;
+void potencia(int* result, int a, int b) {
+  int total = 1;
   for (int i = 0; i < b; i++) {
-    result *= a;
+    total *= a;
   }
-
-  printf("%d\n", result);
+  (*result) = total;
 }
 
 int main() {
-  printf("Calcular potencia de 2^10\n");
+  printf("Calculo de Potencia\n\n");
 
-  potencia(2, 10);
+  int result;
+
+  potencia(&result, 2, 10);
+
+  printf("2^10 = %d\n", result);
 }
