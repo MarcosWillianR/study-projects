2f2920245324faf62d77a58288d08500cbb65b2f
Marcos Willian <markusuuuu@gmail.com>
2022-06-27 01:46:41 -0300

    C study: added hanged algorithm

--- C/forca/forca.c
+++ C/forca/forca.c
@@ -46,13 +46,27 @@ void escolhepalavra() {
   sprintf(palavrasecreta, "MELANCIA");
 }
 
+int enforcou() {
+  int erros = 0;
+  for (int i = 0; i < tentativas; i++) {
+    int existe = 0;
+    for (int j = 0; strlen(palavrasecreta) > j; j++) {
+      if (chutes[i] == palavrasecreta[j]) {
+        existe = 1;
+        break;
+      }
+    }
+    if (!existe) erros++;
+  }
+  return erros >= 5;
+}
+
 int main() {
   int acertou = 0;
-  int enforcou = 0;
   abertura();
   escolhepalavra();
   do {
     desenhaforca();
     chuta();
-  } while (!acertou && !enforcou);
+  } while (!acertou && !enforcou());
 }
