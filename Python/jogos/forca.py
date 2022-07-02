93bf4fe0ec2b20654e678db317e80af9ea563575
Marcos Willian <markusuuuu@gmail.com>
2022-07-02 20:41:28 -0300

    Python study: tuple, dictionary and set introduction

--- Python/jogos/forca.py
+++ Python/jogos/forca.py
@@ -7,17 +7,22 @@ def play():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
 
+    print(f"{' '.join(letras_acertadas)}")
+
     while (not enforcou and not acertou):
         chute = input("Qual letra? ")
-        chute = chute.strip().lower()
+        chute = chute.strip().upper()
 
         for (i, letra) in enumerate(palavra_secreta):
-            if (chute == letra.lower()):
-                print(f"Encontrei a letra {chute} - {i}")
+            if (chute == letra.upper()):
+                letras_acertadas[i] = letra.upper()
+
+        print(f"{' '.join(letras_acertadas)}")
 
 
 if __name__ == "__main__":
