05c990f7c04a9629b2b6772b938d08af831030b8
Marcos Willian <markusuuuu@gmail.com>
2022-07-03 05:59:49 -0300

    Python study: list comprehensions learning and giving more logic to game

--- Python/jogos/forca.py
+++ Python/jogos/forca.py
@@ -6,11 +6,12 @@ def play():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    tentativas = 6
 
     print(f"{' '.join(letras_acertadas)}")
 
@@ -18,11 +19,26 @@ def play():
         chute = input("Qual letra? ")
         chute = chute.strip().upper()
 
-        for (i, letra) in enumerate(palavra_secreta):
-            if (chute == letra.upper()):
-                letras_acertadas[i] = letra.upper()
+        if chute in palavra_secreta:
+            for (i, letra) in enumerate(palavra_secreta):
+                if chute == letra:
+                    letras_acertadas[i] = letra
+        else:
+            tentativas -= 1
+
+        enforcou = tentativas == 0
+        acertou = "_" not in letras_acertadas
 
         print(f"{' '.join(letras_acertadas)}")
+
+    if acertou:
+        print("Parabéns, você ganhou!")
+
+    if enforcou:
+        print("Você perdeu!")
+        print(f"A palavra secreta era {palavra_secreta}")
+
+    print("Fim do jogo.")
 
 
 if __name__ == "__main__":
