383ccf3ca868a870b408b32a6a02c4972a90d35c
Marcos Willian <markusuuuu@gmail.com>
2022-07-11 21:41:59 -0300

    C study: learned dinamic memory allocation with malloc

--- C/foge-foge/main.c
+++ C/foge-foge/main.c
@@ -1,11 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+char** mapa;
+int linhas;
+int colunas;
+
+void liberamapa() {
+  // free -> liberta a memoria alocada dinamicamente
+  for (int i = 0; i < linhas; i++) {
+    free(mapa[i]);
+  }
+  free(mapa);
+}
+
+void alocamapa() {
+  // alocar memoria para o mapa (linhas x colunas) -> alocação dinamica de memoria
+  mapa = malloc(sizeof(char*) * linhas);
+  for (int i = 0; i < linhas; i++) {
+    mapa[i] = malloc(sizeof(char) * (colunas + 1));
+  }
+}
+
+void lemapa() {
+  FILE* f;
+  f = fopen("mapa.txt", "r");
+  if (f == 0) {
+    printf("Erro na leitura do mapa\n");
+    exit(1);
+  }
+  fscanf(f, "%d %d", &linhas, &colunas);
+  alocamapa();
+  // ler o mapa
+  for (int i = 0; i < 5; i++) {
+    fscanf(f, "%s", mapa[i]);
+  }
+  fclose(f);
+}
 
 int main() {
-  // matriz de 5x10
-  char mapa[5][10];
+  lemapa();
 
-  mapa[0][0] = '|';
-  mapa[4][9] = '@';
+  // imprimir o mapa
+  for (int i = 0; i < linhas; i++) {
+    printf("%s\n", mapa[i]);
+  }
 
-  printf("%c %c\n", mapa[0][0], mapa[4][9]);
+  liberamapa();
 }
