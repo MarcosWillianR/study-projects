91ab127053c517278cdd3e5008854519dd8964a8
Marcos Willian <markusuuuu@gmail.com>
2022-07-24 23:57:33 -0300

    ignews - FaunaDB create and get query

--- Javascript/ignews/src/pages/api/auth/[...nextauth].ts
+++ Javascript/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { faunaClient } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,37 @@ export default NextAuth({
       authorization: "https://github.com/login/oauth/authorize?scope=read:user+user:email",
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await faunaClient.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('users_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('users_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 })
