89c9c09cfa767d7d203ecae34899991f08e2ed01
Marcos Willian <markusuuuu@gmail.com>
2022-08-01 01:26:57 -0300

    react native study [First Project]: creating folder structure

---
+++ Javascript/01-fundamentos-react-native/imhere/src/screens/Home/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#131016', padding: 24 },
+  eventName: { color: '#FDFCFE', fontSize: 24, fontWeight: 'bold', marginTop: 48 },
+  eventDate: { color: '#7a7a7a', fontSize: 16 }
+});
+
+export default styles;
