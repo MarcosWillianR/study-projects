dbd8253cccd2f3f9617010f49ee9441320df1c96
Marcos Willian <markusuuuu@gmail.com>
2022-08-08 00:50:59 -0300

    ignews - [STOPPING THIS COURSE FOR BETTER UNDERSTANDING OF THE NEW PRISMIC VERSION]

--- Javascript/ignews/src/pages/_app.tsx
+++ Javascript/ignews/src/pages/_app.tsx
@@ -1,15 +1,30 @@
 import { AppProps } from 'next/app';
+import Link from 'next/link';
 import { SessionProvider } from "next-auth/react"
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { linkResolver, repositoryName } from '../services/prismicio'
 
 import { Header } from '../components/Header';
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Header />
-      <Component {...pageProps} />
-    </SessionProvider>
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, ...props }) => (
+        <Link href={href}>
+          <a {...props} />
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <SessionProvider session={session}>
+          <Header />
+          <Component {...pageProps} />
+        </SessionProvider>
+      </PrismicPreview>
+    </PrismicProvider>
   )
 }
 
