0f46c074414a17d562cb26a17672e1e40931b834
Marcos Willian <markusuuuu@gmail.com>
2022-08-08 00:50:59 -0300

    ignews - [STOPPING THIS COURSE FOR BETTER UNDERSTANDINGMENT OF THE NEW PRISMIC VERSION]

---
+++ Javascript/ignews/.slicemachine/assets/slices/Post/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Post';
+
+export default {
+  title: 'slices/Post'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Orbit","spans":[]}],"description":[{"type":"paragraph","text":"Amet non commodo proident enim sit ad adipisicing duis minim voluptate quis voluptate cillum id. Nostrud aliquip ut aliquip eiusmod velit in minim occaecat adipisicing minim pariatur ut id ipsum. Laborum eu occaecat sunt velit sunt aute et.","spans":[]}]},"slice_type":"post","id":"_Default"}} />
+_Default.storyName = ''
