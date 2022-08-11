8f6f336a43baa4007f273292c4907a483b7333c7
Marcos Willian <markusuuuu@gmail.com>
2022-08-10 23:24:03 -0300

    reactjs fundamentals - css modules learning and created global styles

--- Javascript/01-fundamentos-reactjs/src/App.jsx
+++ Javascript/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+
+import './global.css';
 
 export function App() {
   return (
     <>
+      <Header />
       <Post
         author="Marcos Willian"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, atque, quod, voluptate facilis voluptates impedit esse dignissimos ea architecto necessitatibus perferendis accusantium eos aliquid? Ipsam molestiae officia exercitationem veniam alias."
