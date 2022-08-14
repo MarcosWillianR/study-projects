0f93a6a1105d742e702388dcf9efbc9f2a3170c9
Marcos Willian <markusuuuu@gmail.com>
2022-08-13 23:44:04 -0300

    reactjs fundamentals - sidebar and main content grid structure

--- Javascript/01-fundamentos-reactjs/src/App.jsx
+++ Javascript/01-fundamentos-reactjs/src/App.jsx
@@ -2,15 +2,23 @@ import { Post } from './Post';
 import { Header } from './components/Header';
 
 import './global.css';
+import styles from './App.module.css';
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Marcos Willian"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, atque, quod, voluptate facilis voluptates impedit esse dignissimos ea architecto necessitatibus perferendis accusantium eos aliquid? Ipsam molestiae officia exercitationem veniam alias."
-      />
+
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+
+        <main>
+          <Post
+            author="Marcos Willian"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, atque, quod, voluptate facilis voluptates impedit esse dignissimos ea architecto necessitatibus perferendis accusantium eos aliquid? Ipsam molestiae officia exercitationem veniam alias."
+          />
+        </main>
+      </div>
     </>
   )
 }
