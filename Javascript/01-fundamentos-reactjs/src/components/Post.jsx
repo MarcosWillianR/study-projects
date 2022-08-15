fbc0993f194db2056332eee68bb79c4850c498d5
Marcos Willian <markusuuuu@gmail.com>
2022-08-15 02:16:17 -0300

    reactjs fundamentals - created Avatar component

--- Javascript/01-fundamentos-reactjs/src/components/Post.jsx
+++ Javascript/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,4 +1,5 @@
 import { Comment } from './Comment';
+import { Avatar } from './Avatar';
 
 import styles from './Post.module.css';
 
@@ -7,7 +8,8 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/43147902?v=4" />
+          <Avatar src="https://avatars.githubusercontent.com/u/43147902?v=4" />
+
           <div className={styles.authorInfo}>
             <strong>Marcos Willian</strong>
             <span>React Developer</span>
