524c9c134a5593e9894c9b80779255b4a8095942
Marcos Willian <markusuuuu@gmail.com>
2022-08-15 00:56:59 -0300

    reactjs fundamentals - finish Post structure

---
+++ Javascript/01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,29 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/43147902?v=4" />
+          <div className={styles.authorInfo}>
+            <strong>Marcos Willian</strong>
+            <span>React Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:25">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p>👉 <a href="#">jane.design/doctorcare</a></p>
+
+        <p><a>#novoprojeto #nlw #rocketseat</a></p>
+      </div>
+    </article>
+  )
+}
