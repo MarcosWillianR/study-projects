9e0b65ae577d48c17cfda6dddce3014ed55042a7
Marcos Willian <markusuuuu@gmail.com>
2022-08-27 13:46:07 -0300

    tiktik - finish Footer comp

--- Javascript/tiktik_app/components/Footer.tsx
+++ Javascript/tiktik_app/components/Footer.tsx
@@ -1,5 +1,24 @@
+import { footerList1, footerList2, footerList3 } from '../utils/constants';
+
+const List = ({ data, mt = true }: { data: string[], mt?: boolean }) => {
+  return (
+    <div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
+      {data.map(item => (
+        <p key={item} className="text-gray-400 text-sm hover:underline cursor-pointer">
+          {item}
+        </p>
+      ))}
+    </div>
+  )
+}
+
 export function Footer() {
   return (
-    <div />
+    <div className="mt-6 hidden xl:block">
+      <List data={footerList1} mt={false} />
+      <List data={footerList2} />
+      <List data={footerList3} />
+      <p className="text-gray-400 text-small mt-5">2022 TikTik</p>
+    </div>
   )
 }
