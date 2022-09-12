c61b0772b005d30e4ba2837f84dbc5b6af99fe00
Marcos Willian <markusuuuu@gmail.com>
2022-09-11 21:14:54 -0300

    tiktik - start video details page

--- Javascript/tiktik_app/pages/detail/[id].tsx
+++ Javascript/tiktik_app/pages/detail/[id].tsx
@@ -1,7 +1,25 @@
+import { useState, useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Link from 'next/link';
+import { GoVerified } from 'react-icons/go';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import axios from 'axios';
+
+import { BASE_URL } from '../../utils';
+
 const Detail = () => {
   return (
     <div></div>
   )
+}
+
+export const getServerSideProps = async ({ params: { id } }) => {
+  console.log('id: ', id)
+
+  // const { data } = await axios.get(`${BASE_URL}/api/post/${id}`)
 }
 
 export default Detail;
