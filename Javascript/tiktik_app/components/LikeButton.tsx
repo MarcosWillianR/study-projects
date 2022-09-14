6ed5d6c0e05ca9ecb9e6d97b7575fe9aad8b4748
Marcos Willian <markusuuuu@gmail.com>
2022-09-14 00:29:09 -0300

    tiktik - finish details page

---
+++ Javascript/tiktik_app/components/LikeButton.tsx
@@ -0,0 +1,5 @@
+export function LikeButton() {
+  return (
+    <div></div>
+  )
+}
--- Javascript/tiktik_app/pages/detail/[id].tsx
+++ Javascript/tiktik_app/pages/detail/[id].tsx
@@ -10,6 +10,9 @@ import axios from 'axios';
 
 import { BASE_URL } from '../../utils';
 import { IVideo } from '../../types';
+import { useAuthStore } from '../../store/authStore';
+import { LikeButton } from '../../components/LikeButton';
+import { Comments } from '../../components/Comments';
 
 interface IProps {
   postDetails: IVideo,
@@ -21,6 +24,7 @@ const Detail = ({ postDetails }: IProps) => {
   const [isVideoMuted, setIsVideoMuted] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
   const router = useRouter();
+  const { userProfile } = useAuthStore();
 
   const handleClickVideo = useCallback(() => {
     if (playing) {
@@ -33,7 +37,9 @@ const Detail = ({ postDetails }: IProps) => {
   }, [playing]);
 
   useEffect(() => {
-    if (post && videoRef?.current) videoRef.current.muted = isVideoMuted;
+    if (post && videoRef?.current) {
+      videoRef.current.muted = isVideoMuted;
+    };
   }, [isVideoMuted, post]);
 
   if (!post) return null;
@@ -76,6 +82,50 @@ const Detail = ({ postDetails }: IProps) => {
               <HiVolumeUp />
             </button>
           )}
+        </div>
+      </div>
+
+      <div className="relative w-[1000px] md:w-[900px] lg:w-[700px]">
+        <div className="lg:mt-20 mt-10">
+          <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+            <div className="ml-4 md:w-20 md:h-20 w-16 h-16">
+              <Link href="/">
+                <>
+                  <Image
+                    width={62}
+                    height={62}
+                    className="rounded-full"
+                    src={post.postedBy.image}
+                    alt="profile photo"
+                    layout="responsive"
+                  />
+                </>
+              </Link>
+            </div>
+            <div>
+              <Link href="/">
+                <div className="mt-3 flex flex-col gap-2">
+                  <p className="flex gap-2 md:text-md font-bold text-primary">
+                    {post.postedBy.userName} {' '}
+                    <GoVerified className="text-blue-400 text-md" />
+                  </p>
+                  <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                    {post.postedBy.userName}
+                  </p>
+                </div>
+              </Link>
+            </div>
+          </div>
+
+          <p className="px-10 text-lg text-gray-600">{post.caption}</p>
+
+          <div className="mt-10 px-10">
+            {userProfile && (
+              <LikeButton />
+            )}
+          </div>
+
+          <Comments />
         </div>
       </div>
     </div>
