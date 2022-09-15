68a00079ee54f6974606f951b9880dfffd77608d
Marcos Willian <markusuuuu@gmail.com>
2022-09-14 23:24:43 -0300

    tiktik - start comments component

--- Javascript/tiktik_app/components/Comments.tsx
+++ Javascript/tiktik_app/components/Comments.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+
+import { useAuthStore } from "../store/authStore";
+import { NoResults } from "./NoResults";
+
 export function Comments() {
   return (
     <div></div>
