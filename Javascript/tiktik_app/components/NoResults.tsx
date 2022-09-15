a8c8ad83670a97fe8da38164a7782a7d5daac0c7
Marcos Willian <markusuuuu@gmail.com>
2022-09-15 01:16:12 -0300

    tiktik - finish NoResults component

--- Javascript/tiktik_app/components/NoResults.tsx
+++ Javascript/tiktik_app/components/NoResults.tsx
@@ -1,9 +1,17 @@
+import { BiCommentX } from "react-icons/bi";
+import { MdOutlineVideocamOff } from "react-icons/md";
+
 interface IProps {
   text: string
 }
 
 export function NoResults({ text }: IProps) {
   return (
-    <div />
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className="text-8xl">
+        {text === 'No comments yet!' ? <BiCommentX /> : <MdOutlineVideocamOff />}
+      </p>
+      <p className="text-2xl text-center">{text}</p>
+    </div>
   )
 }
