4049fe98b0f06ae4d049b004dea683379c71e935
Marcos Willian <markusuuuu@gmail.com>
2022-11-21 21:55:16 -0300

    chore: render posts

--- Javascript/01-fundamentos-reactjs/src/components/Post.jsx
+++ Javascript/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,36 +1,46 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import pt_BR from 'date-fns/locale/pt-BR';
+
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'as' HH:mm'h'", { locale: pt_BR });
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, { locale: pt_BR, addSuffix: true })
+
   return (
     <article className={styles.post}>
       <header>
-        <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/43147902?v=4" />
+        <div className={styles.author}>''
+          <Avatar src={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
-            <strong>Marcos Willian</strong>
-            <span>React Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:25">Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map(({ type, content }) => {
+          if (type === 'paragraph') {
+            return (
+              <p>{content}</p>
+            )
+          } else if (type === 'link') {
+            return (
+              <p>
+                <a href="#">{content}</a>
+              </p>
+            )
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
