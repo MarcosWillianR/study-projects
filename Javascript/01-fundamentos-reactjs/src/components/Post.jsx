0a3ee74fc5f9d0f5be2f71eb8498a2ac88e26efb
Marcos Willian <markusuuuu@gmail.com>
2022-12-30 21:25:48 -0300

    chore: retoming react fundamentals

--- Javascript/01-fundamentos-reactjs/src/components/Post.jsx
+++ Javascript/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { format, formatDistanceToNow } from 'date-fns';
 import pt_BR from 'date-fns/locale/pt-BR';
 
@@ -7,13 +8,29 @@ import { Avatar } from './Avatar';
 import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
-  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'as' HH:mm'h'", { locale: pt_BR });
-  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, { locale: pt_BR, addSuffix: true })
+  const [comments, setComments] = useState([]);
+  const [newCommentText, setNewCommentText] = useState('');
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'as' HH:mm'h'", {
+    locale: pt_BR
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: pt_BR,
+    addSuffix: true
+  });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments(state => [...state, newCommentText]);
+    setNewCommentText('');
+  }
 
   return (
     <article className={styles.post}>
       <header>
-        <div className={styles.author}>''
+        <div className={styles.author}>
           <Avatar src={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
@@ -28,25 +45,21 @@ export function Post({ author, publishedAt, content }) {
       </header>
 
       <div className={styles.content}>
-        {content.map(({ type, content }) => {
+        {content.map(({ type, content }, i) => {
           if (type === 'paragraph') {
-            return (
-              <p>{content}</p>
-            )
+            return <p key={content}>{content}</p>
           } else if (type === 'link') {
-            return (
-              <p>
-                <a href="#">{content}</a>
-              </p>
-            )
+            return <p key={content}><a href="#">{content}</a></p>
           }
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
+          onChange={e => setNewCommentText(e.target.value)}
+          value={newCommentText}
           placeholder="Deixe um comentário"
         />
 
@@ -56,9 +69,9 @@ export function Post({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => (
+          <Comment key={comment} />
+        ))}
       </div>
     </article>
   )
