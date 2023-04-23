a8491ee730900384d37027f70f86ce882db39592
Marcos Willian <markusuuuu@gmail.com>
2023-04-23 02:03:13 -0300

    chore: finish Summary component UI

---
+++ Javascript/03-dt-money/src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
+import { Container, Card } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <Card>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </Card>
+
+      <Card>
+        <header>
+          <span>Saidas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </Card>
+
+      <Card variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#ffffff" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </Card>
+    </Container>
+  )
+}
---
+++ Javascript/03-dt-money/src/components/Summary/styles.ts
@@ -0,0 +1,39 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.section`
+  width: 100%;
+  max-width: 1120px;
+  margin: -5rem auto 0;
+  padding: 0 1.5rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 2rem;
+` 
+
+interface CardProps {
+  variant?: 'green';
+}
+
+export const Card = styled.div<CardProps>`
+  background: ${props => props.theme['gray-600']};
+  border-radius: 6px;
+  padding: 2rem;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: ${props => props.theme['gray-300']};
+  }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+  }
+
+  ${props => props.variant === 'green' && css`
+    background: ${props.theme['green-700']};
+  `}
+`
